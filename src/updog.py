a0c029a8b948ad37d5569d9f522f3c101642c791
Nikhil Nair <nik.n.nair@gmail.com>
2017-12-28 10:09:04 -0800

    Support for Meme generation

--- src/updog.py
+++ src/updog.py
@@ -1,26 +1,22 @@
-import os
 import json
-import fbchat
 import random
 import requests
-from fbchat.models import *
+import time
+from PIL import Image, ImageDraw, ImageFont
 
 
-USERNAME = os.getenv('BOT_USER')
-PASS = os.getenv('BOT_PASS')
-GROUP_ID = os.getenv('GROUP_ID')
 YO_MOMMA_URL = "http://api.yomomma.info"
 CHUCK_NORRIS_URL = "http://api.icndb.com/jokes/random"
 
 
 def get_yo_momma():
-    response = requests.get(YO_MOMMA_URL)
+    response = requests.get(YO_MOMMA_URL, timeout=10)
     data = json.loads(response.text)
     return data['joke']
 
 
 def get_chuck_norris():
-    response = requests.get(CHUCK_NORRIS_URL)
+    response = requests.get(CHUCK_NORRIS_URL, timeout=10)
     data = json.loads(response.text)
     return data['value']['joke']
 
@@ -31,19 +27,41 @@ def get_message():
     return data['value']['joke']
 
 
-def send_msg_to_group(client, sources):
-    msg = sources.get(random.choice(list(sources.keys())))()
-    sent = client.send(Message(text=msg), thread_id=GROUP_ID,
-                       thread_type=ThreadType.GROUP)
-    if sent:
-        print("Message sent successfully!")
+def generate_image(choice, msg):
+    if choice == YO_MOMMA_URL:
+        image = Image.open("yomamma.jpg")
+        xy = (0, 235)  # this was found manually
+    elif choice == CHUCK_NORRIS_URL:
+        image = Image.open("chucknorris.jpg")
+        xy = (0, 25)  # this was found manually
+    fnt = ImageFont.truetype('Xpressive Bold.ttf', 20)
+
+    draw = ImageDraw.Draw(image)
+    for line in msg.split(","):
+        draw.text(xy, line, (255, 255, 255), font=fnt)
+        new_y = xy[1] + 20
+        xy = (xy[0], new_y)
+    output_filename = "/tmp/{}.jpg".format(int(time.time()))
+    image.save(output_filename)
+    return output_filename
+
+
+def send_msg_to_group(sources):
+    global YO_MOMMA_URL
+    global CHUCK_NORRIS_URL
+    choice = random.choice(list(sources.keys()))
+    msg = sources.get(choice)()
+    processed_image = generate_image(choice, msg)
+    return processed_image
 
 
 def handler(event, context):
-    client = fbchat.Client(USERNAME, PASS)
     sources = {YO_MOMMA_URL: get_yo_momma,
                CHUCK_NORRIS_URL: get_chuck_norris}
-    send_msg_to_group(client, sources)
+    image_file = send_msg_to_group(sources)
+    with open(image_file, 'rb') as fp:
+        image_contents = fp.readlines()
+    return image_contents
 
 
 if __name__ == "__main__":
